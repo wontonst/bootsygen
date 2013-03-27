b3fd1df711f34865b5b8dbb3c73da4329aa81cd7
Roy Zheng <roycraft3@gmail.com>
2013-03-27 15:13:40 -0700

    large refactor, looks better, no bugs yet

--- lib/website.php
+++ lib/website.php
@@ -39,41 +39,48 @@ class Website {
     }
 
     public function build() {
-        $file = fopen('out/index.html', 'w+');
-        foreach ($this->metadata as $k => $v) {//this is not pointless - it sets the variables for index.html
-            $$k = $v;
-        }
+      $this->buildIndex();
+
         $pages = $this->pages;
+        foreach($this->pages as $p) {
+	  $this->buildPage($p);
+        }
+    }
+/**
+builds index.html (outputs to index.html)
+ */
+    private function buildIndex()
+    {
+        $file = fopen($this->metadata['output'].'/index.html', 'w+');
+        foreach ($this->metadata as $k => $v) {//this is not pointless - it sets the variables for including /modules/body.php
+	  $$k = $v;
+        }
+        $navbar = Navbar::create($this->pages, 'index');
         $previews = $this->createPreviews();
         ob_start();
-        $navbar = Navbar::create($pages, 'index');
         include(__DIR__ . '/../modules/body.php');
         $index = ob_get_clean();
         fwrite($file, $index);
-        foreach($this->pages as $p) {
+	fclose($file);
+    }
+    /**
+builds a single page (outputs to file)
+     */
+    private function buildPage(&$p)
+    {
+        foreach ($this->metadata as $k => $v) {//this is not pointless - it sets the variables for /modules/body.php
+	  $$k = $v;
+        }
             ob_start();
-            fclose($file);
             $file = fopen($GLOBALS['CONFIG']['output']. '/'.$p->getTitle().'.html','w+');
             $title=$p->getTitle();
             $content = $p->getContent();
             if($p->hasDescription())$description=$p->getDescription();
-            $navbar = Navbar::create($pages,$title);
+            $navbar = Navbar::create($this->pages,$title);
             include(__DIR__.'/../modules/page.php');
             if(!$index=ob_get_clean())throw new Exception('website.php ob_get_clean() has encountered an issue at line '.__LINE__);
             fwrite($file,$index);
-        }
-    }
-    private function buildIndex()
-    {
-
-    }
-    private function buildAllPages()
-    {
-
-    }
-    private function buildPage()
-    {
-
+	    fclose($file);
     }
     private function createPreviews() {
       $array=array();
